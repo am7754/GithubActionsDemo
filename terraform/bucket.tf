156d6a2d0211889e2d1400d9e3dc40d76fee38ad
AM7754 <am7754@zebra.com>
2024-08-22 16:41:43 +0530

    New Ashfaq Website

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -10,7 +10,7 @@ resource "google_storage_bucket" "static-site" {
     not_found_page   = "404.html"
   }
   cors {
-    origin          = ["http://image-store.com"]
+    origin          = ["http://ashfaq.com"]
     method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
     response_header = ["*"]
     max_age_seconds = 3600
