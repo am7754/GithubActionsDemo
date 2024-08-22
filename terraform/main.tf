ae6346ea6b9bfcae8c7a3400a29a8df707d37e01
AM7754 <am7754@zebra.com>
2024-08-22 16:27:15 +0530

    Format Terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
 terraform {
-    backend "gcs" { 
-      bucket  = "terraform-state-ashfaq-cicd-project"
-      prefix  = "dev"
-    }
+  backend "gcs" {
+    bucket = "terraform-state-ashfaq-cicd-project"
+    prefix = "dev"
+  }
 }
 
 provider "google" {
   project = var.project
-  region = var.region
+  region  = var.region
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,22 +1,22 @@
 locals {
-    labels = {
-        "data-project" = var.data-project
-    }
+  labels = {
+    "data-project" = var.data-project
+  }
 }
 
 variable "project" {
-    type= string
-    description = "ID Google project"
-	default = "festive-shield-433206-t8"
+  type        = string
+  description = "ID Google project"
+  default     = "festive-shield-433206-t8"
 }
 
 variable "region" {
-    type= string
-    description = "Region Google project"
-	default = "us-central1"
+  type        = string
+  description = "Region Google project"
+  default     = "us-central1"
 }
 
-variable  "data-project" {
-    type = string
-    description = "Name of data pipeline project to use as resource prefix"
+variable "data-project" {
+  type        = string
+  description = "Name of data pipeline project to use as resource prefix"
 }
